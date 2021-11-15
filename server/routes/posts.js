3001f1f9a21906873aa5c009f1835f85ee47b9df
Bryan <cortinabryan@gmail.com>
2021-11-15 12:44:19 -0800

    Added Schema, Routing & Controllers

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
 
+import { getPosts, createPost } from "../controllers/posts";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("THIS WORKS!");
-});
+router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
